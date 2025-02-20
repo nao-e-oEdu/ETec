a865d0433aadd1e63c20784e9ff4d38f7931d4fd
Eduardo Mina <eduardomina2008@gmail.com>
2025-02-20 09:09:44 -0300

    Files from 20/02 class

---
+++ Pendrive/BD2/Aula 2/BD_Colegio.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE BD_Colegio;
+USE BD_Colegio;
+
+CREATE TABLE disciplinas (
+	CodDisciplina CHAR(2) PRIMARY KEY,
+	NomeDisciplina VARCHAR(30)
+);
+
+CREATE TABLE cursos (
+	codcurso CHAR(2) PRIMARY KEY,
+	nome VARCHAR(50),
+	coddisc1 CHAR(2) FOREIGN KEY REFERENCES disciplinas(CodDisciplina),
+	coddisc2 CHAR(2) FOREIGN KEY REFERENCES disciplinas(CodDisciplina),
+	coddisc3 CHAR(2) FOREIGN KEY REFERENCES disciplinas(CodDisciplina)
+);
+
+CREATE TABLE alunos (
+	matricula VARCHAR(5) PRIMARY KEY,
+	nome VARCHAR(50),
+	endereco VARCHAR(50),
+	cidade VARCHAR(30),
+	codcurso CHAR(2) FOREIGN KEY REFERENCES cursos(codcurso)
+);
+
+INSERT INTO disciplinas (CodDisciplina, NomeDisciplina) VALUES
+('11', 'Banco de Dados'),
+('12', 'Lógica de Programação'),
+('13', 'Desenvolvimento de Software 1'),
+('21', 'Banco de Dados 2'),
+('22', 'Desenvolvimento de Software 2'),
+('23', 'Programação de Computadores 1'),
+('31', 'Banco de Dados 3'),
+('32', 'Programação de Computadores 2'),
+('33', 'Desenvolvimento de Software 3');
+
+INSERT INTO cursos (codcurso, nome, coddisc1, coddisc2, coddisc3) VALUES
+('01', 'Auxiliar de Informática', '11', '12', '13'),
+('02', 'Programador de Computadores', '21', '22', '23'),
+('03', 'Técnico em Informática', '31', '32', '33');
+
+INSERT INTO alunos (matricula, nome, endereco, cidade, codcurso) VALUES
+('10001', 'Marcos Moraes', 'Rua Pe Roque, 2057', 'Mogi Mirim', '01'),
+('10002', 'Maria Conceição Lopes', 'Rua Araras, 23', 'Mogi Guaçu', '01'),
+('10003', 'Ana Carina Lopes', 'Rua Peraltas, 222', 'Santos', '01'),
+('10004', 'Carlos Aguiar', 'Rua Botafogo, 33', 'Santos', '01'),
+('10005', 'André Luiz dos Santos', 'Rua Lopes Conte, 3343', 'Sapucai', '01'),
+('10006', 'Pedro Antonio Pimenta', 'Rua Altair Lopes, 33', 'Itapira', '02'),
+('10007', 'Rita de Cássia da Silva', 'Rua Eletromais, 33', 'Itapira', '02'),
+('10008', 'Caique dos Santos', 'Rua das Amoreiras, 55', 'Campinas', '02'),
+('10009', 'Carlos Tavares', 'Rua Peixe, 99', 'Santos', '02'),
+('10010', 'Antonio Carlos Caetano', 'Rua Josefina Alface, 907', 'Campinas', '02'),
+('10011', 'Ricardo Moreira', 'Rua do Pinhal, 332', 'Aparecida', '03'),
+('10012', 'Richardson S. P. Campeao', 'Rua do Tricolor, 433', 'São Paulo', '03'),
+('10013', 'Junior Camisa Seis', 'Rua do Morumbi, 433', 'São Paulo', '03'),
+('10014', 'Carina Melo', 'Rua Osvaldo Ramos, 88', 'Mogi Guaçu', '03'),
+('10015', 'Pedro Mello', 'Rua Itororó, 3999', 'Mogi Mirim', '03');
